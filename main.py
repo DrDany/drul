fee8dc2a695cce4fac07abd09e14c36d4f77b34f
Daniil Druzhinin <dany@wisebits.com>
2020-09-08 19:06:57 +0300

    fix

--- main.py
+++ main.py
@@ -1,20 +1,31 @@
 from openpyxl import load_workbook
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--surname", type=str, required=True, help="second name", action="store", dest="surname")
+args = parser.parse_args()
+surname = args.surname
+surname_upper = surname.upper()
+file_name = surname + '.xlsx'
+result = list(surname_upper)
+
+
+
+
 wb = load_workbook('./exel.xlsx')
-print(wb.get_sheet_names())
 
 sheet = wb.get_sheet_by_name('стр.1')
 
-K16 = sheet['K16'].value
-print(K16)
 
-sheet['W16'] = 'Ф'
-sheet['AA16'] = 'E'
-sheet['AE16'] = 'Д'
-sheet['AI16'] = 'О'
-sheet['AM16'] = 'Р'
-sheet['AQ16'] = 'Е'
-sheet['AU16'] = 'Н'
-sheet['AY16'] = 'К'
-sheet['BC16'] = 'О'
-
-wb.save('fedorenko.xlsx')
+#
+sheet['W16'] = surname_upper[0]
+sheet['AA16'] = surname_upper[1]
+sheet['AE16'] = surname_upper[2]
+sheet['AI16'] = surname_upper[3]
+sheet['AM16'] = surname_upper[4]
+sheet['AQ16'] = surname_upper[5]
+# # sheet['AU16'] = surname_upper[6]
+# # sheet['AY16'] = 'К'
+# # sheet['BC16'] = 'О'
+# #
+   #           wb.save(file_name)
