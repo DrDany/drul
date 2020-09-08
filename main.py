ff0a0a551bc8d780eea0bd69312dea5a65d5d170
dani <dany@wisebits.com>
2020-09-08 21:38:06 +0300

    add list

--- main.py
+++ main.py
@@ -9,23 +9,15 @@ surname_upper = surname.upper()
 file_name = surname + '.xlsx'
 result = list(surname_upper)
 
-
+list = ('W16', 'AA16', 'AE16', 'AI16', 'AM16', 'AQ16', 'AU16', 'AY16', 'BC16', 'BG16', 'BK16', 'BO16', 'BS16', 'BW16', 'CA16', 'CE16', 'CI16', 'CM16','CQ16')
 
 
 wb = load_workbook('./exel.xlsx')
 
 sheet = wb.get_sheet_by_name('стр.1')
 
+for letter in range(len(surname)):
+    sheet[list[letter]] = surname_upper[letter]
 
-#
-sheet['W16'] = surname_upper[0]
-sheet['AA16'] = surname_upper[1]
-sheet['AE16'] = surname_upper[2]
-sheet['AI16'] = surname_upper[3]
-sheet['AM16'] = surname_upper[4]
-sheet['AQ16'] = surname_upper[5]
-# sheet['AU16'] = surname_upper[6]
-# sheet['AY16'] = 'К'
-# sheet['BC16'] = 'О'
 #
 wb.save(file_name)
